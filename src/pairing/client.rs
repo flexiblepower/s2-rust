9f87ded5fc1b99654f54f8f992ae502ff74c549b
David Venhoek <david@tweedegolf.com>
2026-02-09 14:40:09 +0100

    Refactored client in preparation for error handlign.

--- src/pairing/client.rs
+++ src/pairing/client.rs
@@ -14,14 +14,14 @@ pub struct PairingRemote {
 
 pub async fn pair(config: Config, remote: PairingRemote, pairing_token: &[u8], role: Role) -> PairingResult<Pairing> {
     let client = reqwest::Client::new();
-    let pairing_version = negotiate_version(remote.url.clone(), &client).await?;
+    let pairing_version = negotiate_version(&client, remote.url.clone()).await?;
 
     match pairing_version {
-        PairingVersion::V1 => pair_v1(config, remote, pairing_token, role, client).await,
+        PairingVersion::V1 => pair_v1(client, remote, config, pairing_token, role).await,
     }
 }
 
-async fn negotiate_version(url: Url, client: &reqwest::Client) -> Result<PairingVersion, Error> {
+async fn negotiate_version(client: &reqwest::Client, url: Url) -> Result<PairingVersion, Error> {
     let response = client.get(url).send().await.unwrap();
     let status = response.status();
 
@@ -41,11 +41,11 @@ async fn negotiate_version(url: Url, client: &reqwest::Client) -> Result<Pairing
 }
 
 async fn pair_v1(
-    config: Config,
+    client: reqwest::Client,
     remote: PairingRemote,
+    config: Config,
     pairing_token: &[u8],
     role: Role,
-    client: reqwest::Client,
 ) -> PairingResult<Pairing> {
     let base_url = remote.url.join("v1/").unwrap();
 
@@ -54,10 +54,137 @@ async fn pair_v1(
 
     let client_hmac_challenge = HmacChallenge::new(&mut rand::rng());
 
+    let request_pairing_response = v1_request_pairing(&client, &base_url, config, remote.id, &client_hmac_challenge).await?;
+    let attempt_id = request_pairing_response.pairing_attempt_id;
+
+    match request_pairing_response.selected_hmac_hashing_algorithm {
+        HmacHashingAlgorithm::Sha256 => {
+            let expected = client_hmac_challenge.sha256(&network, pairing_token);
+
+            if expected != request_pairing_response.client_hmac_challenge_response {
+                let _ = v1_finalize(&client, &base_url, &attempt_id, false).await;
+                return Err(Error::InvalidToken);
+            }
+        }
+    }
+
+    let server_hmac_challenge_response = match request_pairing_response.selected_hmac_hashing_algorithm {
+        HmacHashingAlgorithm::Sha256 => request_pairing_response.server_hmac_challenge.sha256(&network, pairing_token),
+    };
+
+    let pairing = match role {
+        Role::CommunicationServer {
+            initiate_connection_url,
+            access_token,
+        } => {
+            if let Err(e) = v1_post_connection_details(
+                &client,
+                &base_url,
+                &attempt_id,
+                server_hmac_challenge_response,
+                initiate_connection_url.clone().into(),
+                access_token.clone(),
+            )
+            .await
+            {
+                let _ = v1_finalize(&client, &base_url, &attempt_id, false).await;
+                return Err(e);
+            }
+            Pairing {
+                remote_endpoint_description: request_pairing_response.server_s2_endpoint_description,
+                remote_node_description: request_pairing_response.server_s2_node_description,
+                token: access_token,
+                role: PairingRole::CommunicationServer,
+            }
+        }
+        Role::CommunicationClient => {
+            let connection_details = match v1_get_connection_details(&client, &base_url, &attempt_id, server_hmac_challenge_response).await
+            {
+                Ok(connection_details) => connection_details,
+                Err(e) => {
+                    let _ = v1_finalize(&client, &base_url, &attempt_id, false).await;
+                    return Err(e);
+                }
+            };
+            Pairing {
+                remote_endpoint_description: request_pairing_response.server_s2_endpoint_description,
+                remote_node_description: request_pairing_response.server_s2_node_description,
+                token: connection_details.access_token,
+                role: PairingRole::CommunicationClient {
+                    initiate_url: connection_details.initiate_connection_url,
+                },
+            }
+        }
+    };
+
+    v1_finalize(&client, &base_url, &attempt_id, true).await?;
+
+    Ok(pairing)
+}
+
+async fn v1_get_connection_details(
+    client: &reqwest::Client,
+    base_url: &Url,
+    attempt_id: &PairingAttemptId,
+    server_hmac_challenge_response: HmacChallengeResponse,
+) -> PairingResult<ConnectionDetails> {
+    let request = RequestConnectionDetailsRequest {
+        server_hmac_challenge_response,
+    };
+    let response = client
+        .post(base_url.join("requestConnectionDetails").unwrap())
+        .header(PairingAttemptId::HEADER_NAME, attempt_id.header_value())
+        .json(&request)
+        .send()
+        .await
+        .unwrap();
+    if response.status() != StatusCode::OK {
+        todo!()
+    }
+    let connection_details = response.json::<ConnectionDetails>().await.unwrap();
+    Ok(connection_details)
+}
+
+async fn v1_post_connection_details(
+    client: &reqwest::Client,
+    base_url: &Url,
+    attempt_id: &PairingAttemptId,
+    server_hmac_challenge_response: HmacChallengeResponse,
+    initiate_connection_url: String,
+    access_token: AccessToken,
+) -> PairingResult<()> {
+    let request = PostConnectionDetailsRequest {
+        server_hmac_challenge_response,
+        connection_details: ConnectionDetails {
+            initiate_connection_url,
+            access_token,
+        },
+    };
+    let response = client
+        .post(base_url.join("postConnectionDetails").unwrap())
+        .header(PairingAttemptId::HEADER_NAME, attempt_id.header_value())
+        .json(&request)
+        .send()
+        .await
+        .unwrap();
+    if response.status() != StatusCode::NO_CONTENT {
+        todo!()
+    }
+
+    Ok(())
+}
+
+async fn v1_request_pairing(
+    client: &reqwest::Client,
+    base_url: &Url,
+    config: Config,
+    id: S2NodeId,
+    client_hmac_challenge: &HmacChallenge,
+) -> PairingResult<RequestPairingResponse> {
     let request = RequestPairing {
         node_description: config.node_description.clone(),
         endpoint_description: config.endpoint_description.clone(),
-        id: remote.id,
+        id,
         supported_protocols: vec![CommunicationProtocol::WebSocket],
         supported_versions: config.supported_protocol_versions.clone(),
         supported_hashing_algorithms: vec![HmacHashingAlgorithm::Sha256],
@@ -74,84 +201,10 @@ async fn pair_v1(
         todo!()
     }
     let request_pairing_response = response.json::<RequestPairingResponse>().await.unwrap();
-    let attempt_id = request_pairing_response.pairing_attempt_id;
-
-    match request_pairing_response.selected_hmac_hashing_algorithm {
-        HmacHashingAlgorithm::Sha256 => {
-            let expected = client_hmac_challenge.sha256(&network, pairing_token);
-
-            if expected != request_pairing_response.client_hmac_challenge_response {
-                let _ = v1_finalize(&attempt_id, &base_url, &client, false).await;
-                return Err(Error::InvalidToken);
-            }
-        }
-    }
-
-    let server_hmac_challenge_response = match request_pairing_response.selected_hmac_hashing_algorithm {
-        HmacHashingAlgorithm::Sha256 => request_pairing_response.server_hmac_challenge.sha256(&network, pairing_token),
-    };
-
-    let pairing = match role {
-        Role::CommunicationServer {
-            initiate_connection_url,
-            access_token,
-        } => {
-            let request = PostConnectionDetailsRequest {
-                server_hmac_challenge_response,
-                connection_details: ConnectionDetails {
-                    initiate_connection_url: initiate_connection_url.clone().into(),
-                    access_token: access_token.clone(),
-                },
-            };
-            let response = client
-                .post(base_url.join("postConnectionDetails").unwrap())
-                .header(PairingAttemptId::HEADER_NAME, attempt_id.header_value())
-                .json(&request)
-                .send()
-                .await
-                .unwrap();
-            if response.status() != StatusCode::NO_CONTENT {
-                todo!()
-            }
-            Pairing {
-                remote_endpoint_description: request_pairing_response.server_s2_endpoint_description,
-                remote_node_description: request_pairing_response.server_s2_node_description,
-                token: access_token,
-                role: PairingRole::CommunicationServer,
-            }
-        }
-        Role::CommunicationClient => {
-            let request = RequestConnectionDetailsRequest {
-                server_hmac_challenge_response,
-            };
-            let response = client
-                .post(base_url.join("requestConnectionDetails").unwrap())
-                .header(PairingAttemptId::HEADER_NAME, attempt_id.header_value())
-                .json(&request)
-                .send()
-                .await
-                .unwrap();
-            if response.status() != StatusCode::OK {
-                todo!()
-            }
-            let connection_details = response.json::<ConnectionDetails>().await.unwrap();
-            Pairing {
-                remote_endpoint_description: request_pairing_response.server_s2_endpoint_description,
-                remote_node_description: request_pairing_response.server_s2_node_description,
-                token: connection_details.access_token,
-                role: PairingRole::CommunicationClient {
-                    initiate_url: connection_details.initiate_connection_url,
-                },
-            }
-        }
-    };
-
-    v1_finalize(&attempt_id, &base_url, &client, true).await?;
-
-    Ok(pairing)
+    Ok(request_pairing_response)
 }
 
-async fn v1_finalize(attempt_id: &PairingAttemptId, url: &Url, client: &reqwest::Client, success: bool) -> PairingResult<()> {
+async fn v1_finalize(client: &reqwest::Client, url: &Url, attempt_id: &PairingAttemptId, success: bool) -> PairingResult<()> {
     let response = client
         .post(url.join("finalizePairing").unwrap())
         .header(PairingAttemptId::HEADER_NAME, attempt_id.header_value())
