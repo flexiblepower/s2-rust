3930768a66d2ebaa41b005e546da8f088c88a45c
Wester Coenraads <wester.coenraads@tno.nl>
2026-02-11 13:49:58 +0100

    Experiment with D-Bus.

---
+++ src/main.rs
@@ -0,0 +1,67 @@
+#![cfg(feature = "dbus")]
+use futures_util::StreamExt;
+use std::time::Duration;
+use zbus::{Connection, fdo::DBusProxy, names::OwnedBusName, proxy};
+
+struct DBusServer {
+    connection: Connection,
+    pending_names: Vec<OwnedBusName>,
+}
+
+impl DBusServer {
+    async fn new() -> eyre::Result<Self> {
+        let connection = Connection::system().await?;
+        let dbus = DBusProxy::new(&connection).await?;
+        let pending_names = dbus.list_names().await?;
+        Ok(Self { connection, pending_names })
+    }
+
+    async fn receive_connection(&mut self) -> eyre::Result<String> {
+        while let Some(name) = self.pending_names.pop() {
+            // TODO: check the name
+            S2RmProxy::builder(&self.connection).destination(name).unwrap().build().await.unwrap();
+        }
+
+        let dbus = DBusProxy::new(&self.connection).await?;
+        let changed = dbus.receive_name_owner_changed().await?.next().await.ok_or(eyre::eyre!("oh no"))?;
+        dbg!(changed.args().unwrap());
+        // Ok(changed.to_string())
+        Ok("ok".to_string())
+    }
+}
+
+#[proxy(default_path = "/S2/0/Rm", interface = "com.victronenergy.S2")]
+trait S2Rm {
+    fn discover(&self) -> zbus::Result<bool>;
+    fn connect(&self, cem_id: String, keep_alive_internal: i32) -> zbus::Result<bool>;
+    fn disconnect(&self, cem_id: String) -> zbus::Result<()>;
+    fn message(&self, cem_id: String, message: String) -> zbus::Result<()>;
+    fn keep_alive(&self, cem_id: String) -> zbus::Result<bool>;
+
+    #[zbus(signal)]
+    fn message(cem_id: String, message: String);
+    #[zbus(signal)]
+    fn disconnect(cem_id: String, reason: String);
+}
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    if std::env::var("SERVER").map(|var| var == "true").unwrap_or(false) {
+        let mut server = DBusServer::new().await?;
+        loop {
+            server.receive_connection().await?;
+        }
+    } else {
+        let connection = Connection::system().await?;
+        println!("Sleeping for 2 seconds.");
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        println!("Acquiring name.");
+        connection.request_name("nl.westercoenraads.S2").await?;
+        tokio::time::sleep(Duration::from_secs(2)).await;
+        println!("Releasing name.");
+        connection.release_name("nl.westercoenraads.S2").await?;
+        // S2RmProxy::new(conn, destination)
+    }
+
+    Ok(())
+}
--- src/transport/dbus.rs
+++ src/transport/dbus.rs
@@ -1,3 +1,19 @@
-//! D-Bus support. Currently unimplemented.
-//!
-//! D-Bus support will be implemented here, likely according to [this description](https://github.com/victronenergy/venus/wiki/Venus-OS-D%E2%80%90Bus-S2-Interface). It is not currently available.
+// //! D-Bus support.
+// //!
+// //! D-Bus will be supported here according to [this description](https://github.com/victronenergy/venus/wiki/Venus-OS-D%E2%80%90Bus-S2-Interface).
+
+// #[cfg(test)]
+// async fn connect() -> eyre::Result<()> {
+
+// }
+
+// #[cfg(test)]
+// #[test]
+// fn test() {
+//     // Create the runtime
+//     let rt = tokio::runtime::Runtime::new().unwrap();
+
+//     // Execute the future, blocking the current thread until completion
+//     rt.block_on(connect()).unwrap();
+//     panic!()
+// }
