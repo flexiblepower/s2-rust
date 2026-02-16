d7e677f60ed5757a29eb5579b00d4cb9351d812f
David Venhoek <david@tweedegolf.com>
2026-02-16 10:06:14 +0100

    Clippy and formatting fixes for existing code.

--- src/transport.rs
+++ src/transport.rs
@@ -1,10 +1,10 @@
 //! Abstractions over transport protocols, and their implementations.
-//! 
+//!
 //! The S2 specification does not mandate a specific transport protocol. This module provides the primary way
 //! transport protocols are abstracted in this crate: [`S2Transport`]. Implementing this trait for your
 //! desired transport protocol allows you to use all of the connection types in this library (like [`S2Connection`](crate::connection::S2Connection))
 //! with that transport protocol.
-//! 
+//!
 //! In addition, this module provides specific transport protocol implementations in its submodules.
 //! The most relevant of these is [`websockets_json`], which provides an implementation for JSON over
 //! WebSockets according to [the official JSON schema](https://github.com/flexiblepower/s2-ws-json).
@@ -34,7 +34,7 @@ pub trait S2Transport {
     fn receive(&mut self) -> impl Future<Output = Result<Message, Self::TransportError>> + Send;
 
     /// Disconnect this connection.
-    /// 
+    ///
     /// This should do whatever is appropriate for the implemented transport protocol. This may include sending
     /// e.g. a close frame. When the future resolves, the connection should be fully terminated.
     fn disconnect(self) -> impl Future<Output = ()> + Send;
@@ -44,9 +44,9 @@ pub trait S2Transport {
 // So for now it's just unconditionally public (and it might be useful for other people doing tests, so maybe that's fine?).
 #[doc(hidden)]
 pub mod test {
-    use std::convert::Infallible;
-    use crate::{connection::S2Connection, frbc::StorageStatus};
     use super::*;
+    use crate::{connection::S2Connection, frbc::StorageStatus};
+    use std::convert::Infallible;
 
     pub struct MockTransport;
 
